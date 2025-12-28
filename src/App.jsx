28d87af6a471235a186198200c568c750a1bc997
STAP16 <stepancat61@gmail.com>
2025-12-28 12:23:22 +0300

    change page to first

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,11 @@ import RegisterPageWoYup from './pages/RegisterPageWoYup'
 import RegisterPageWithHookForm from './pages/RegisterPageWithHookForm'
 
 function App() {
-	const [page, setPage] = useState("second")
+	const [page, setPage] = useState('first')
 	return (
 		<>
-		{page === "first" && <RegisterPageWoYup setPage={setPage} />}
-		{page === "second" &&<RegisterPageWithHookForm setPage={setPage} />}
+			{page === 'first' && <RegisterPageWoYup setPage={setPage} />}
+			{page === 'second' && <RegisterPageWithHookForm setPage={setPage} />}
 		</>
 	)
 }
